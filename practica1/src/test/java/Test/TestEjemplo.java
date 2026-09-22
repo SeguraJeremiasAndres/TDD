bab89613a4f6bf090603a51130339f51d7b6447f
SeguraJeremiasAndres <jeremiasnobsegura@gmail.com>
2026-09-22 00:54:24 -0300

    arreglo de carpetas faltantes y depencias en el pom

---
+++ practica1/src/test/java/Test/TestEjemplo.java
@@ -0,0 +1,10 @@
+package Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class TestEjemplo {
+    @Test
+
+    public void TestPrueba() {
+        assertEquals(2,1+1);
+    }
+}
